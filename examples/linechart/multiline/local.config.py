06cc6b0f79477685ff281b2b1181e4cc5cc0fc63
Adria Armejach <miconof@gmail.com>
2016-09-23 17:18:51 +0200

    Add example for linecharts

---
+++ examples/linechart/multiline/local.config.py
@@ -0,0 +1,21 @@
+chart_type = "linechart"
+
+figure_size = (10,10)
+
+title=''
+xtitle=''
+ytitle='Test'
+
+bbox=(0.305,0.)
+legend_ncol=1
+legend_loc=3
+
+# ylim = (0, 8.0)
+
+# do_add_geomean = True
+
+do_labels = True
+
+marker_patterns = ('o','s','d','^','*','+')
+line_styles = ('-', '--', '-.', ':', 'steps', ' ')
+colors = ('r', 'k', 'b', 'r', 'k', 'b')
